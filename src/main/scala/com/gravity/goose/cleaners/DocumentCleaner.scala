d9f4f03fba711f8abf26a5b3ece4df0efe6efdcd
teresy <hi.teresy@gmail.com>
2018-11-05 22:50:28 -0500

    remove unneeded parentheses in if guards

--- src/main/scala/com/gravity/goose/cleaners/DocumentCleaner.scala
+++ src/main/scala/com/gravity/goose/cleaners/DocumentCleaner.scala
@@ -72,8 +72,7 @@ trait DocumentCleaner {
     for {
       node <- ems
       images: Elements = node.getElementsByTag("img")
-      if (images.size == 0)
-    } {
+      if images.size == 0 } {
       val tn: TextNode = new TextNode(node.text, doc.baseUri)
       node.replaceWith(tn)
     }
